f84a531ac97eab2a427eb45a7f7c017910073149
ghifar1327 <givartorreto@gmail.com>
2026-02-26 13:51:05 +0700

    feat: adds feature get update data user based by id

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func main() {
 			Message: "backend is running well",
 		})
 	})
-// ================================================================================== Create User
+	// ================================================================================== Create User
 	r.POST("/users", func(ctx *gin.Context) {
 		var data Users
 
@@ -67,9 +67,9 @@ func main() {
 			Results: listener,
 		})
 	})
-	
-// ================================================================================================ GET User
-	
+
+	// ================================================================================================ GET User
+
 	r.GET("/users/:id", func(ctx *gin.Context) {
 		idParam := ctx.Param("id")
 
@@ -92,9 +92,65 @@ func main() {
 			}
 		}
 		ctx.JSON(400, Response{
-		Success: false,
-		Message: "User not found",
+			Success: false,
+			Message: "User not found",
+		})
 	})
+	// ======================================================================================== Update User
+
+	r.PATCH("/users/:id", func(ctx *gin.Context) {
+		idParam := ctx.Param("id")
+		id, err := strconv.Atoi(idParam)
+		if err != nil {
+			ctx.JSON(400, Response{
+				Success: false,
+				Message: "Invalid ID",
+			})
+			return
+		}
+		var updateUser Users
+		if err := ctx.ShouldBindJSON(&updateUser); err != nil {
+			ctx.JSON(400, Response{
+				Success: false,
+				Message: "Invalid request",
+			})
+		}
+
+		for i, user := range listener {
+			if user.Id == id {
+				// -----------------------------------------------------------------------------------------------------
+				if updateUser.Email != "" && updateUser.Email != user.Email {
+					for _, u := range listener {
+						if u.Email == updateUser.Email {
+							ctx.JSON(400, Response{
+								Success: false,
+								Message: "Email already exists",
+							})
+							return
+						}
+					}
+					listener[i].Email = updateUser.Email
+				}
+				// ---------------------------------------------------------------------------------------------------------
+				if updateUser.Password != "" {
+					listener[i].Password = updateUser.Password
+				}
+
+				ctx.JSON(200, Response{
+					Success: true,
+					Message: "User updated successfully",
+					Results: listener[i],
+				})
+				return
+			}
+		}
+
+		ctx.JSON(404, Response{
+			Success: false,
+			Message: "User not found",
+		})
 	})
+
+	// ================================================================================================================================== delete user
 	r.Run("localhost:8888")
 }
