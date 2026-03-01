e5ebeb440e79c416652bfb582f54fbd676559f0e
ghifar1327 <givartorreto@gmail.com>
2026-03-01 15:34:00 +0700

    feat: add AddChart endpoint with price calculation and in-memory cart storage

--- handlers/orderHandle.go
+++ handlers/orderHandle.go
@@ -16,6 +16,8 @@ type ProductResponse struct {
 	Methods   []string `json:"methods"`
 }
 
+var Cart []models.CartItem
+
 func GetProducts(ctx *gin.Context) {
 	defer mu.Unlock()
 	mu.Lock()
@@ -34,4 +36,56 @@ func GetProducts(ctx *gin.Context) {
 		})
 	}
 	ctx.JSON(200, Response{true, "List of Product", result})
+}
+
+func AddChart(ctx *gin.Context) {
+	defer mu.Unlock()
+	mu.Lock()
+	
+	var input models.CartItem
+	if err := ctx.ShouldBindJSON(&input); err != nil {
+		ctx.JSON(404, gin.H{"error": "invalid request body"})
+		return
+	}
+
+	// =========================================================== search product
+	var product *models.Product
+	for i := range models.Products {
+		if models.Products[i].Id == input.ProductID {
+			product = &models.Products[i]
+			break
+		}
+	}
+
+	if product == nil {
+		ctx.JSON(404, gin.H{"error": "Product not found"})
+	}
+
+	// =========================================================== add price
+	var variantPrice, sizePrice, methodPrice int
+
+	for _, v := range models.Variants {
+		if v.Id == input.VariantID {
+			variantPrice = v.AddPrice
+			break
+		}
+	}
+
+	for _, s := range models.Sizes {
+		if s.Id == input.SizeID {
+			sizePrice = s.AddPrice
+			break
+		}
+	}
+	for _, m := range models.Methods {
+		if m.Id == input.MethodID {
+			methodPrice = m.AddPrice
+		}
+	}
+	totalPrice := (variantPrice + sizePrice + methodPrice + product.BasePrice) * input.Qty
+	input.Id = len(Cart) + 1
+	input.Price = totalPrice
+
+	Cart = append(Cart, input)
+	ctx.JSON(200, Response{true, "Product added successfuly", Cart})
 }
