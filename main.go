8c00ea1892e972e87b0cb49f55952c85c44e0fe1
ghifar1327 <givartorreto@gmail.com>
2026-03-02 13:26:21 +0700

    fix: fixed typo

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func AuthMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		ctx.Header("Access-Control-Allow-Origin", "http://localhost:5173/")
+		ctx.Header("Access-Control-Allow-Origin", "http://localhost:5173")
 		ctx.Header("Access-Control-Allow-Headers", "content-type")
 		if ctx.Request.Method == "OPTIONS" {
 			ctx.Data(http.StatusOK, "", []byte(""))
