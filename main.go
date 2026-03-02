3e38f824f27eb1447077dff421cbb6e95ec9fc7f
ghifar1327 <givartorreto@gmail.com>
2026-03-03 01:11:03 +0700

    fix: fixed middlware cors

--- main.go
+++ main.go
@@ -2,36 +2,22 @@ package main
 
 import (
 	"backend/handlers"
-	"net/http"
+	"backend/middleware"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
-func AuthMiddleware() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		ctx.Header("Access-Control-Allow-Origin", "http://localhost:5173")
-		ctx.Header("Access-Control-Allow-Headers", "content-type")
-		if ctx.Request.Method == "OPTIONS" {
-			ctx.Data(http.StatusOK, "", []byte(""))
-		} else {
-			ctx.Next()
-		}
-	}
-}
-
 func main() {
+	godotenv.Load()
 	r := gin.Default()
-
-	auth := r.Group("/")
-	auth.Use(AuthMiddleware())
-	{
-		r.GET("/users", handlers.GetUsers)
-		r.POST("/register", handlers.Register)
-		r.POST("/login", handlers.Login)
-		r.GET("/users/:id", handlers.GetUserByID)
-		r.PATCH("/updateuser/:id", handlers.UpdateUser)
-		r.DELETE("/deleteuser/:id", handlers.DeleteUser)
-	}
+	r.Use(middleware.CorsMiddleware())
+	r.GET("/users", handlers.GetUsers)
+	r.POST("/register", handlers.Register)
+	r.POST("/login", handlers.Login)
+	r.GET("/users/:id", handlers.GetUserByID)
+	r.PATCH("/updateuser/:id", handlers.UpdateUser)
+	r.DELETE("/deleteuser/:id", handlers.DeleteUser)
 
 	// ================================================================ product order
 	r.GET("/products", handlers.GetProducts)
