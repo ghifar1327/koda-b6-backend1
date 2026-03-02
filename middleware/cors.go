657698349fa079ee245b5cda904b99a60cb37577
ghifar1327 <givartorreto@gmail.com>
2026-03-03 01:13:13 +0700

    feat: add CORS middleware for frontend integration

---
+++ middleware/cors.go
@@ -0,0 +1,19 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CorsMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		ctx.Header("Access-Control-Allow-Origin", "http://localhost:5173")
+		ctx.Header("Access-Control-Allow-Headers", "content-type")
+		if ctx.Request.Method == "OPTIONS" {
+			ctx.Data(http.StatusOK, "", []byte(""))
+		} else {
+			ctx.Next()
+		}
+	}
+}
