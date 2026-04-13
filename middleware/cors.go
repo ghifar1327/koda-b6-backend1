5a7655fbc87e454a703ba55dd76379738992bc01
ghifar1327 <givartorreto@gmail.com>
2026-04-13 11:46:26 +0700

    fix: cors middleware

--- middleware/cors.go
+++ middleware/cors.go
@@ -2,18 +2,25 @@ package middleware
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func CorsMiddleware() gin.HandlerFunc {
+	// godotenv.Load()
 	return func(ctx *gin.Context) {
-		ctx.Header("Access-Control-Allow-Origin", "http://localhost:5173")
-		ctx.Header("Access-Control-Allow-Headers", "content-type")
+		origin := os.Getenv("FRONTEND_URL")
+	
+		ctx.Header("Access-Control-Allow-Origin", origin)
+		ctx.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		ctx.Header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
+
 		if ctx.Request.Method == "OPTIONS" {
-			ctx.Data(http.StatusOK, "", []byte(""))
-		} else {
-			ctx.Next()
+			ctx.AbortWithStatus(http.StatusOK)
+			return
 		}
+
+		ctx.Next()
 	}
 }
